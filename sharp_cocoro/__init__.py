6de1877eaa599901d5b34f183d325fb28785834c
David Mohl <git@d.sh>
2025-06-27 15:49:29 +0900

    Bump version

--- sharp_cocoro/__init__.py
+++ sharp_cocoro/__init__.py
@@ -4,4 +4,4 @@ from .devices.aircon.aircon import Aircon
 from .devices.purifier.purifier import Purifier
 from .properties import DeviceType, ValueType, SinglePropertyStatus, RangePropertyType, BinaryPropertyStatus, RangePropertyStatus
 
-__all__ = ['Cocoro', 'Device', 'Aircon', 'Purifier', 'DeviceType', 'StatusCode', 'ValueSingle', 'ValueType', 'SinglePropertyStatus', 'RangePropertyType', 'BinaryPropertyStatus', 'RangePropertyStatus']
+__all__ = ['Cocoro', 'Device', 'Aircon', 'Purifier', 'DeviceType', 'ValueType', 'SinglePropertyStatus', 'RangePropertyType', 'BinaryPropertyStatus', 'RangePropertyStatus']
